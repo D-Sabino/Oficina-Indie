cea7c61830548ed1d09149da79a01c4d52d861a7
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2024-09-26 23:17:41 -0300

    Desafio1

---
+++ MyProject/Assets/Desafio1/Desafio1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desafio1 : MonoBehaviour
+{
+
+    public int X;
+    public int Y;
+    
+
+
+    void Start()
+    {
+        int soma = X + Y;
+        Debug.Log("A soma de " + X + " + " + Y + " é igual a " + soma);
+    }
+
+
+    void Update()
+    {
+
+    }
+}
